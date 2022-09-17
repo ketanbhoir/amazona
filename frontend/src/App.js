d4c7e2bdcd11b134404ba94c2899571b6ee8e514
ketanbhoir <619ketanbhoir@gmail.com>
2022-09-17 13:55:05 +0530

    05-list-products-updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,16 @@ function App() {
         {/* a classname of products & product 4 arrange */}
         <div className="products">
           {data.products.map((product) => (
+            // A slug is the part of a URL that identifies a particular page on a website in an easy-to-read form.
             // creatin a unique key for child element in list
             <div className="product" key={product.slug}>
-              <img src={product.image} alt={product.name} />
+              {/* link the img with products details pg */}
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
               <div className="product-info">
+                {/* link the p.name with products details pg */}
                 <a href={`/product/${product.slug}`}>
                   <p>{product.name}</p>
                 </a>
